f2eae68ccb1a9e1c88ecf83470ca2e0db0d60908
Nikita253 <np790328@gmail.com>
2023-09-05 18:57:22 +0530

    Project added

---
+++ Showroom.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+public class Showroom implements utility{
+    String showroom_name;
+    String showroom_address;
+    int total_employees;
+    int total_cars_in_stock = 0;
+    String manager_name;
+
+    @Override
+    public void get_details() {
+        System.out.println("Showroom name : "+showroom_name);
+        System.out.println("Showroom Address : "+showroom_address);
+        System.out.println("Manager name : "+manager_name);
+        System.out.println("Total Employees : "+total_employees);
+        System.out.println("Total cars in stock : "+total_cars_in_stock);
+    }
+
+    @Override
+    public void set_details(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("===================================== *** ENTER SHOWROOM DETAILS *** =======================================");
+        System.out.println();
+        System.out.println("SHOWROOM NAME : ");
+        showroom_name = sc.nextLine();
+        System.out.println("SHOWROOM ADDRESS : ");
+        showroom_address = sc.nextLine();
+        System.out.println("MANAGER NAME : ");
+        manager_name = sc.nextLine();
+        System.out.println("TOTAL NO OF EMPLOYEES : ");
+        total_employees = sc.nextInt();
+        System.out.println("TOTAL CARS IN STOCK : ");
+        total_cars_in_stock = sc.nextInt();
+    }
+}
---
+++ utility.java
@@ -0,0 +1,8 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+
+
+interface utility {
+   void get_details();
+
+   void set_details();
+}
